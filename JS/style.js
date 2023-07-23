6e3210df441754b6ace0d16a357f2a680f18fa4b
Zoyace Shrestha <Zoyace.Shrestha@macbookpro-c02fn2y3md6t.lan>
2023-07-23 15:37:51 -0400

    completed webpage

--- JS/style.js
+++ JS/style.js
@@ -1,10 +1,13 @@
 const commercialBtn = document.querySelector("#Commercial");
+const medicaidBtn = document.querySelector("#Medicaid");
+const medicareBtn = document.querySelector("#Medicare");
 const hiddenBlock = document.querySelector(".hidden-chart-container ");
 const navBar = document.querySelector(".navbar-container-mobile-view");
 const navBarIcon = document.querySelector(".navbar-hamburger-icon");
 const searchBtn = document.querySelector(".CommercialSearch");
 const coverContainer = document.querySelector(".hidden-cover-container");
 const nonCoverContainer = document.querySelector(".hidden-non-cover-container");
+const cologuardimage = document.querySelector(".cologuard-image");
 
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
@@ -18,13 +21,42 @@ function toggleMobileMenu(menu) {
 //   }
 // });
 
+// Commercial button
 commercialBtn.addEventListener("click", () => {
   if (hiddenBlock.style.display === "none") {
     hiddenBlock.style.display = "grid";
+    cologuardimage.style.display = "none";
   } else {
     hiddenBlock.style.display = "none";
     coverContainer.style.display = "none";
     nonCoverContainer.style.display = "none";
+    cologuardimage.style.display = "block";
+  }
+});
+
+// medicacid
+medicaidBtn.addEventListener("click", () => {
+  if (hiddenBlock.style.display === "none") {
+    hiddenBlock.style.display = "grid";
+    cologuardimage.style.display = "none";
+  } else {
+    hiddenBlock.style.display = "none";
+    coverContainer.style.display = "none";
+    nonCoverContainer.style.display = "none";
+    cologuardimage.style.display = "block";
+  }
+});
+
+// Medicare btn
+medicareBtn.addEventListener("click", () => {
+  if (hiddenBlock.style.display === "none") {
+    hiddenBlock.style.display = "grid";
+    cologuardimage.style.display = "none";
+  } else {
+    hiddenBlock.style.display = "none";
+    coverContainer.style.display = "none";
+    nonCoverContainer.style.display = "none";
+    cologuardimage.style.display = "block";
   }
 });
 
