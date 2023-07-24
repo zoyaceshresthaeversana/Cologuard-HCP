afe6930d4b62f8e91bf2b901d5469333d427d293
Zoyace Shrestha <Zoyace.Shrestha@macbookpro-c02fn2y3md6t.lan>
2023-07-24 16:22:59 -0400

    finished website

--- JS/style.js
+++ JS/style.js
@@ -2,9 +2,17 @@ const commercialBtn = document.querySelector("#Commercial");
 const medicaidBtn = document.querySelector("#Medicaid");
 const medicareBtn = document.querySelector("#Medicare");
 const hiddenBlock = document.querySelector(".hidden-chart-container ");
+const medicaidHiddenBlock = document.querySelector(
+  ".Medicaid-hidden-chart-container"
+);
+const medicareHiddenBlock = document.querySelector(
+  ".Medicare-hidden-chart-container"
+);
 const navBar = document.querySelector(".navbar-container-mobile-view");
 const navBarIcon = document.querySelector(".navbar-hamburger-icon");
 const searchBtn = document.querySelector(".CommercialSearch");
+const medicacidSearchBtn = document.querySelector(".MedicaidSearch");
+const medicareSearchBtn = document.querySelector(".MedicareSearch");
 const coverContainer = document.querySelector(".hidden-cover-container");
 const nonCoverContainer = document.querySelector(".hidden-non-cover-container");
 const cologuardimage = document.querySelector(".cologuard-image");
@@ -23,10 +31,18 @@ function toggleMobileMenu(menu) {
 
 // Commercial button
 commercialBtn.addEventListener("click", () => {
+  medicaidBtn.classList.remove("activebtn");
+  medicareBtn.classList.remove("activebtn");
+  medicaidHiddenBlock.style.display = "none";
+  medicareHiddenBlock.style.display = "none";
+  coverContainer.style.display = "none";
+  nonCoverContainer.style.display = "none";
   if (hiddenBlock.style.display === "none") {
+    commercialBtn.classList.add("activebtn");
     hiddenBlock.style.display = "grid";
     cologuardimage.style.display = "none";
   } else {
+    commercialBtn.classList.remove("activebtn");
     hiddenBlock.style.display = "none";
     coverContainer.style.display = "none";
     nonCoverContainer.style.display = "none";
@@ -36,11 +52,19 @@ commercialBtn.addEventListener("click", () => {
 
 // medicacid
 medicaidBtn.addEventListener("click", () => {
-  if (hiddenBlock.style.display === "none") {
-    hiddenBlock.style.display = "grid";
+  medicareBtn.classList.remove("activebtn");
+  commercialBtn.classList.remove("activebtn");
+  hiddenBlock.style.display = "none";
+  medicareHiddenBlock.style.display = "none";
+  coverContainer.style.display = "none";
+  nonCoverContainer.style.display = "none";
+  if (medicaidHiddenBlock.style.display === "none") {
+    medicaidBtn.classList.add("activebtn");
+    medicaidHiddenBlock.style.display = "grid";
     cologuardimage.style.display = "none";
   } else {
-    hiddenBlock.style.display = "none";
+    medicaidBtn.classList.remove("activebtn");
+    medicaidHiddenBlock.style.display = "none";
     coverContainer.style.display = "none";
     nonCoverContainer.style.display = "none";
     cologuardimage.style.display = "block";
@@ -49,11 +73,19 @@ medicaidBtn.addEventListener("click", () => {
 
 // Medicare btn
 medicareBtn.addEventListener("click", () => {
-  if (hiddenBlock.style.display === "none") {
-    hiddenBlock.style.display = "grid";
+  commercialBtn.classList.remove("activebtn");
+  medicaidBtn.classList.remove("activebtn");
+  hiddenBlock.style.display = "none";
+  medicaidHiddenBlock.style.display = "none";
+  coverContainer.style.display = "none";
+  nonCoverContainer.style.display = "none";
+  if (medicareHiddenBlock.style.display === "none") {
+    medicareBtn.classList.add("activebtn");
+    medicareHiddenBlock.style.display = "grid";
     cologuardimage.style.display = "none";
   } else {
-    hiddenBlock.style.display = "none";
+    medicareBtn.classList.remove("activebtn");
+    medicareHiddenBlock.style.display = "none";
     coverContainer.style.display = "none";
     nonCoverContainer.style.display = "none";
     cologuardimage.style.display = "block";
@@ -65,10 +97,38 @@ searchBtn.addEventListener("click", () => {
   var selectedProvider = document.getElementById("Provider").value;
   console.log(selectedCounty);
   console.log(selectedProvider);
-  if (selectedCounty == "QA" && selectedProvider == "EBB") {
+  if (selectedProvider == "T") {
     nonCoverContainer.style.display = "none";
     coverContainer.style.display = "block";
-  } else if (selectedCounty == "QA" && selectedProvider == "MP") {
+  } else if (selectedProvider == "F") {
+    nonCoverContainer.style.display = "block";
+    coverContainer.style.display = "none";
+  } else {
+  }
+});
+
+medicacidSearchBtn.addEventListener("click", () => {
+  var selectedMedicaidProvider =
+    document.getElementById("medicaidProvider").value;
+  console.log(selectedMedicaidProvider);
+  if (selectedMedicaidProvider == "T") {
+    nonCoverContainer.style.display = "none";
+    coverContainer.style.display = "block";
+  } else if (selectedMedicaidProvider == "F") {
+    nonCoverContainer.style.display = "block";
+    coverContainer.style.display = "none";
+  } else {
+  }
+});
+
+medicareSearchBtn.addEventListener("click", () => {
+  var selectedMedicaidProvider =
+    document.getElementById("medicareProvider").value;
+  console.log(selectedMedicaidProvider);
+  if (selectedMedicaidProvider == "T") {
+    nonCoverContainer.style.display = "none";
+    coverContainer.style.display = "block";
+  } else if (selectedMedicaidProvider == "F") {
     nonCoverContainer.style.display = "block";
     coverContainer.style.display = "none";
   } else {
